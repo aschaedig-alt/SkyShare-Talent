5f9876d18afd34c406f31009ef25d60400ec7451
SkyShare Developer <developer@skyshare.local>
2026-06-09 00:12:53 -0600

    Remove SQLite Prisma adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../prisma/generated/client/client";
+import { PrismaClient } from "@prisma/client";
 
 const databaseUrl = process.env.DATABASE_URL;
 
